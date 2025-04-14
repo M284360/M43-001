cd3d255cd6a4513941d507ffc5d8639448731f51
M43 <168723634+M284360@users.noreply.github.com>
2025-04-15 08:15:29 +0900

    Update sample.php

--- sample.php
+++ sample.php
@@ -1,1 +1,12 @@
 <?php echo $_POST["confirm"]; ?>
+if ( !array_key_exists(‘comment’, $_POST){
+
+  print “コメントは必須項目です。”;
+
+}
+if ( str_len($_POST[‘comment’]) > 255 ) {
+
+  print “コメントは２５５文字以下で入力してください。”;
+
+}
+ 
